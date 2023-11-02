9bb1e6e276a647f83292103c3dfdc5ff77c0e242
A M i T <email.mrjarvis@gmail.com>
2023-11-03 00:58:37 +0530

    Create calculator2.0.py

---
+++ calculator2.0.py
@@ -0,0 +1,47 @@
+print("====================================")
+print("| Welcome to the Calculator Program |")
+print("====================================")
+
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    if num2 == 0:
+        return "Error: Division by zero"
+    return num1 / num2
+
+def calculator():
+    print("Select operation you want to perform:")
+    print("1. Addition\n2. Subtraction\n3. Multiplication\n4. Division")
+
+    choice = input("Enter choice (1/2/3/4): ")
+    print("You have selected:", choice)
+
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    if choice == '1':
+        result = add(num1, num2)
+        print(num1, "+", num2, "=", result)
+    elif choice == '2':
+        result = subtract(num1, num2)
+        print(num1, "-", num2, "=", result)
+    elif choice == '3':
+        result = multiply(num1, num2)
+        print(num1, "*", num2, "=", result)
+    elif choice == '4':
+        result = divide(num1, num2)
+        print(num1, "/", num2, "=", result)
+    else:
+        print("Invalid input")
+        return
+
+    print("Thank you for using the calculator")
+
+calculator()
